277d7aaacdbdd919ad789802eab2d4c22fe0ef94
Jan Kaisrlik <jan.kaisrlik@intel.com>
2023-10-19 00:26:07 +0200

    qemu: add other way how to provide qcow and qemu

--- machine/qemu/flake.nix
+++ machine/qemu/flake.nix
@@ -1,6 +1,26 @@
 {
-	outputs = { self, nixpkgs, }:
-	{
+	inputs = {
+		nixpkgs.url = "nixpkgs/nixos-unstable";
+		nixos-generators = {
+			url = "github:nix-community/nixos-generators";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
+	};
+
+	outputs = { self, nixpkgs, nixos-generators, ... }:
+	let
+		system = "x86_64-linux";
+		pkgs = nixpkgs.legacyPackages.${system};
+		my_qemu = pkgs.qemu.overrideAttrs (old: {
+			version = "git";
+			src = pkgs.fetchFromGitHub {
+				owner = "";
+				repo = "";
+				rev = "";
+				sha256 = pkgs.lib.fakeSha256;
+			};
+		});
+	in {
 		nixosModules.base = {pkgs, ...}: {
 			imports = [
 					./configuration.nix
@@ -15,6 +35,21 @@
 			modules = [
 				self.nixosModules.base
 				self.nixosModules.vm
+			];
+		};
+
+		packages.x86_64-linux = {
+			qcow = nixos-generators.nixosGenerate {
+				system = "x86_64-linux";
+				modules = [
+					./configuration.nix
+				];
+				format = "qcow";
+			};
+		};
+		devShells.x86_64-linux.default = pkgs.mkShell {
+			packages = with pkgs; [
+				my_qemu
 			];
 		};
 	};
