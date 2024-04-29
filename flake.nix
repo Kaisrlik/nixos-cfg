820d607557149a1248273c4a28e2e7692e651b44
Jan Kaisrlik <ja.kaisrlik@gmail.com>
2024-04-29 20:07:51 +0200

    nifixy

--- flake.nix
+++ flake.nix
@@ -1,9 +1,12 @@
 {
 	description = "Xeris NIXOS";
 
-	inputs.nixpkgs.url = "nixpkgs/nixos-unstable-small";
+	inputs = {
+		nixpkgs.url = "nixpkgs/nixos-unstable-small";
+		nixify-cfg.url = "/home/jkaisrli/devel/nixos-cfg/nixify-cfg";
+	};
 
-	outputs = { self, nixpkgs }:
+	outputs = { self, nixpkgs, nixify-cfg, ... }:
 		let
 			system = "x86_64-linux";
 			lib = nixpkgs.lib;
@@ -14,7 +17,11 @@
 			nixosConfigurations = {
 				intel = lib.nixosSystem {
 					inherit system;
-					modules = [ ./machine/intel/configuration.nix ];
+					inherit (nixpkgs) lib;
+					modules = [
+						./machine/intel/configuration.nix
+						nixify-cfg.nixosModules.intelize
+					];
 				};
 				beast = lib.nixosSystem {
 					inherit system;
