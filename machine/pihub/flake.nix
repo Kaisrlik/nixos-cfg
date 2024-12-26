937e557c2ed65ff9d109fccf99c4d5217cbd1106
Jan Kaisrlik <jan.kaisrlik@intel.com>
2024-12-26 12:44:18 +0100

    pihub: add inital configuration

---
+++ machine/pihub/flake.nix
@@ -0,0 +1,88 @@
+{
+  description = "Pi Hub";
+
+  nixConfig = {
+    # Only during the first build, otherwise I don't want to allow such a binary cache
+    extra-substituters = [ "https://nix-community.cachix.org" ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+  };
+
+  outputs = { self, nixpkgs, nixos-hardware }:
+    let
+      inherit (nixpkgs.lib) nixosSystem;
+      basic-config = { pkgs, lib, ... }: {
+        # bcm2711 for rpi 3, 3+, 4, zero 2 w
+        # bcm2712 for rpi 5
+        # See the docs at:
+        # https://www.raspberrypi.com/documentation/computers/linux_kernel.html#native-build-configuration
+        # raspberry-pi-nix.board = "bcm2712";
+        time.timeZone = "Europe/Prague";
+        users.users.root = {
+          initialPassword = "root";
+        };
+        # Define a user account. Don't forget to set a password with ‘passwd’.
+        users.users.xeri = {
+          isNormalUser = true;
+          extraGroups = [ "wheel" ];
+          initialPassword = "xeri";
+          openssh.authorizedKeys.keys = [
+            # beast
+            "AAAAB3NzaC1yc2EAAAADAQABAAABgQCqo5qym7gpyFrfoUzWW8v48ovVL/oI5TC0wdhnH0ZTp1OpAhWUlF96EHHmVCi0Agrx5Blf86/hbx07vzzrS+MVsg+dRkaaaMcn3JoBrUykvkttey8SseRFJJmP4+Upu1fyBv3CzVqrn5SZ2jhw2Ti1EbCz82gVIeA5cJhRb+i+VckEOfkOQT4VnS175b+U5+/TZl3jfTf9cyoVXziaS7jHEh2/yWE60Cdzdbje6VZuxM/zVSf7TZsQ/tTqnP7uWEDC9d0La7cfSHzYZnh2mULpcr1M6F+gukIZHk2UOhuPiC/SIk3B1xElPz1azi3QxW0KKRI4gvgplh6Ft+jVbtkYQHVE0cvrFu98EuUTm96kg/dQ1T6O5XohyKop930MMic5raNdcAXj4a0eYrN9GnMklYUhP2XQO3b+z81QgSO3dXBJyjeX1oazEpx6kAPaNYpsleyJO6gXjAnZpRyUypQUayKTttsg7XtPKwKWYpjsZtC771AJfptiBdKd4a+xGVk="
+          ];
+        };
+
+        boot.loader.generic-extlinux-compatible.enable = true;
+        boot.loader.systemd-boot.enable = false;
+        boot.loader.efi.canTouchEfiVariables = false;
+
+        networking = {
+          hostName = "pihub";
+        };
+        environment.systemPackages = with pkgs; [
+          vim
+          wget
+        ];
+        services.openssh = {
+          enable = true;
+        };
+        system.stateVersion = "24.11";
+      };
+    in
+      {
+        nixosConfigurations = {
+          pihub = nixpkgs.lib.nixosSystem {
+            system = "aarch64-linux";
+            modules = [
+              basic-config
+              ({ pkgs, modulesPath, ... }: {
+                nixpkgs = {
+                  localSystem = {
+                    system = "x86_64-linux";
+                  };
+                  crossSystem = {
+                    system = "aarch64-linux";
+                  };
+                # workaround for breaking linux kernel changes https://github.com/NixOS/nixpkgs/issues/154163#issuecomment-1350599022
+                 overlays = [
+                    (final: super: {
+                       makeModulesClosure = x: super.makeModulesClosure (x // { allowMissing = true; });
+                     })
+                 ];
+               }; # nixpkgs end
+               imports = [
+                 nixos-hardware.nixosModules.raspberry-pi-5
+                 "${toString modulesPath}/installer/sd-card/sd-image-aarch64.nix"
+               ];
+            })
+          ];
+        };
+      };
+    };
+}
