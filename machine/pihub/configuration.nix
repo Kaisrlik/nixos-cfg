9f7db91fc72fc8517758b5d0dc3263a44833b1a1
Jan Kaisrlik <jan.kaisrlik@intel.com>
2025-01-29 23:01:51 +0100

    pihub: move hw configuration to hardware-configuration.nix

--- machine/pihub/configuration.nix
+++ machine/pihub/configuration.nix
@@ -7,33 +7,10 @@ let
   username = "xeri";
 in {
   imports = [
+      ./hardware-configuration.nix
 #     <nixpkgs/nixos/modules/profiles/headless.nix>
 #     <nixpkgs/nixos/modules/profiles/minimal.nix>
   ];
-
-  hardware.raspberry-pi.config.all = {
-    options = {
-      # Automatically load overlays for detected cameras
-      camera_auto_detect.enable = false;
-      # Disable compensation for displays with overscan
-      disable_overscan.enable = false;
-      # Automatically load overlays for detected DSI displays
-      display_auto_detect.enable = false;
-    };
-    dt-overlays.vc4-kms-v3d.enable = false;
-    base-dt-params = {
-      # Enable the PCIe external connector
-      pciex1.enable = true;
-      # Force Gen 3.0 speeds
-      # WARNING The Raspberry Pi 5 is not certified for Gen 3.0 speeds, and
-      # connections to PCIe devices at these speeds may be unstable. You should
-      # then reboot your Raspberry Pi for these settings to take effect.
-      pciex1_gen = {
-        enable = true;
-        value = 3;
-      };
-    };
-  };
 
   # only add strictly necessary modules
   # boot.initrd.includeDefaultModules = false;
@@ -43,12 +20,6 @@ in {
   #   <nixpkgs/nixos/modules/profiles/all-hardware.nix>
   #   <nixpkgs/nixos/modules/profiles/base.nix>
   # ];
-  boot.initrd.availableKernelModules = [
-    "nvme"
-    "usb_storage"
-    "usbhid"
-    "xhci_pci"
-  ];
 
   # disable useless software
   environment.defaultPackages = [];
@@ -56,7 +27,6 @@ in {
   xdg.mime.enable   = false;
   xdg.sounds.enable = false;
 
-  raspberry-pi-nix.board = "bcm2712";
   time.timeZone = "Europe/Prague";
   users.users.root = {
       initialPassword = "root";
---
+++ machine/pihub/hardware-configuration.nix
@@ -0,0 +1,35 @@
+{ pkgs, ... }:
+{
+  hardware.raspberry-pi.config.all = {
+    options = {
+      # Automatically load overlays for detected cameras
+      camera_auto_detect.enable = false;
+      # Disable compensation for displays with overscan
+      disable_overscan.enable = false;
+      # Automatically load overlays for detected DSI displays
+      display_auto_detect.enable = false;
+    };
+    dt-overlays.vc4-kms-v3d.enable = false;
+    base-dt-params = {
+      # Enable the PCIe external connector
+      pciex1.enable = true;
+      # Force Gen 3.0 speeds
+      # WARNING The Raspberry Pi 5 is not certified for Gen 3.0 speeds, and
+      # connections to PCIe devices at these speeds may be unstable. You should
+      # then reboot your Raspberry Pi for these settings to take effect.
+      pciex1_gen = {
+        enable = true;
+        value = 3;
+      };
+    };
+  };
+
+  raspberry-pi-nix.board = "bcm2712";
+
+  boot.initrd.availableKernelModules = [
+    "nvme"
+    "usb_storage"
+    "usbhid"
+    "xhci_pci"
+  ];
+}
