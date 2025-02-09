60af1dc60d4b2f313825140719497ab68da55935
Jan Kaisrlik <jan.kaisrlik@intel.com>
2025-02-09 10:50:28 +0100

    GE-Proton: uppdate to 9.24

--- pkgs/proton-ge-custom/default.nix
+++ pkgs/proton-ge-custom/default.nix
@@ -3,11 +3,11 @@ with pkgs;
 
 stdenv.mkDerivation rec {
   pname = "proton-ge-custom";
-  version = "GE-Proton9-18";
+  version = "GE-Proton9-24";
 
   src = fetchurl {
     url = "https://github.com/GloriousEggroll/proton-ge-custom/releases/download/${version}/${version}.tar.gz";
-    sha256 = "sha256-aw2o8/ATJ1yHaz7pxLE91kzOQnCgqjqiMUZL9g1To8s=";
+    sha256 = "sha256-Gdm5Y2I17evWa7UHLCHdrtwHHwzCvyIwRlopBMStDdw=";
   };
 
   buildCommand = ''
