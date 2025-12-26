d8ffb4a6fcc1b93188d6ff7ec454b78d132d5591
Jan Kaisrlik <ja.kaisrlik@gmail.com>
2025-12-26 20:47:35 +0100

    beast: updated hw config

--- machine/beast/hardware-configuration.nix
+++ machine/beast/hardware-configuration.nix
@@ -8,7 +8,7 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "vmd" "xhci_pci" "ahci" "nvme" "usbhid" ];
+  boot.initrd.availableKernelModules = [ "vmd" "xhci_pci" "ahci" "nvme" "usb_storage" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
