b7f3dceef9e7a9814b554594a9bf2fe8e24e7553
Jan Kaisrlik <jan.kaisrlik@intel.com>
2026-07-28 20:38:45 +0200

    sway: fix issue around screensharing

--- modules/sway.nix
+++ modules/sway.nix
@@ -1,5 +1,18 @@
 { pkgs, ... }:
 {
+  nixpkgs.overlays = [
+    (final: prev: {
+      xdg-desktop-portal-wlr = prev.xdg-desktop-portal-wlr.overrideAttrs (oldAttrs: {
+        version = "0.8.4";
+        src = oldAttrs.src.override {
+          rev = "v0.8.4";
+          sha256 = "sha256-8Ohgkz13FcG8ddjjgreXkvFD2Q+zUDZnAM4Oh+C9P/s=";
+        };
+      });
+    })
+  ];
+
+
   services.displayManager.ly.enable = true;
 
   # Enable touchpad support (enabled default in most desktopManager).
@@ -18,7 +31,6 @@
       i3blocks
       mako # notification daemon
       sysstat # cpu_stats script
-      slurp grim # screen capturing
     ];
   };
 
@@ -28,11 +40,11 @@
     wlr = {
       enable = true;
       settings.screencast = {
-        max_fps = 30;
+        # max_fps = 30;
         chooser_type = "dmenu";
         chooser_cmd = "${pkgs.bemenu}/bin/bemenu -l 12 --center --width-factor 0.5 -p \"Share window:\" -i -B 3";
-        exec_before = "${pkgs.swaynotificationcenter}/bin/swaync-client --dnd-on";
-        exec_after = "${pkgs.swaynotificationcenter}/bin/swaync-client --dnd-off";
+        # exec_before = "${pkgs.swaynotificationcenter}/bin/swaync-client --dnd-on";
+        # exec_after = "${pkgs.swaynotificationcenter}/bin/swaync-client --dnd-off";
       };
     };
     extraPortals = with pkgs; [
@@ -41,9 +53,14 @@
     ];
   };
 
-  security.polkit.enable = true;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    pulse.enable = true;
+    wireplumber.enable = true;
+  };
 
-  services.pipewire.enable = true;
   environment.sessionVariables = {
     MOZ_ENABLE_WAYLAND = "1";
     XDG_CURRENT_DESKTOP = "sway";
